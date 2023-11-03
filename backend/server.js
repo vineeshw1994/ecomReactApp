6127469203036366c20036e8fefb5cc62e8c6bad
vineesh <vineeshflorida@gmail.com>
2023-11-03 13:03:10 +0530

    set the final port

--- backend/server.js
+++ backend/server.js
@@ -30,21 +30,34 @@ app.get('/api/config/paypal', (req, res) =>
 );
 
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname,'/uploads')));
-if (process.env.NODE_ENV === 'production') {
+// const __dirname = path.resolve();
+// app.use('/uploads', express.static(path.join(__dirname,'/uploads')));
+// if (process.env.NODE_ENV === 'production') {
  
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+//   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) =>
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//   );
+// } else {
+//   // const __dirname = path.resolve();
+//   // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+//   app.get('/', (req, res) => {
+//     res.send('API is running....');
+//   });
+// }
+
+if(process.env.NODE_ENV === 'production'){
+  const  __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*',(req,res) =>{
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-} else {
-  // const __dirname = path.resolve();
-  // app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  })
+} else{
   app.get('/', (req, res) => {
-    res.send('API is running....');
-  });
+         res.send('API is running....');
+      });
 }
 
 
