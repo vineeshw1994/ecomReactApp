0eb28f75a631bb43f1c1c4cc72d24b45d0895e18
vineesh <vineeshflorida@gmail.com>
2023-11-03 11:57:55 +0530

    Final_File

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect('mongodb+srv://hiringhrbot39:meenakshi1998@cluster0.vroolz4.mongodb.net/pro?retryWrites=true&w=majority');
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;  
 
 connectDB();
 
